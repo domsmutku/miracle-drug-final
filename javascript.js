427317c95ab373238e2d8a1ed27d820b0bc3ceae
domsmutku <zientek.nataliaa@gmail.com>
2026-04-23 21:11:11 +0200

    Add JS alert to purchase and meta data

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function clicked() {
+  if (
+    alert(
+      "Transaction complete. $560.99 dollars have been withdrawn from your bank account. Please remain where The Company can find you.",
+    )
+  ) {
+    yourformelement.submit();
+  }
+}
